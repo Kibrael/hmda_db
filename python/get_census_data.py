d817cf2bfb337f36a8a1636b6554eb3b7d03f96b
= <breeroell@gmail.com>
2020-03-18 09:34:32 -0600

    update files

--- python/get_census_data.py
+++ python/get_census_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[21]:
+# In[2]:
 
 
 
@@ -14,7 +14,7 @@ import shutil
 import pandas as pd
 import requests
 import zipfile
-DATA_PATH = "../data/"
+DATA_PATH = "../data/census/"
 if not os.path.exists(DATA_PATH):
     os.makedirs(DATA_PATH)
 
@@ -43,6 +43,11 @@ def get_ffiec_census_file(years=[2019]):
         print("saving data for {year} as {name}".format(year=year, name=local_file_name))
         with open("../data/"+local_file_name, "wb") as infile:
             infile.write(census_resp.content)
+
+
+# In[ ]:
+
+
 year_list = range(1990,2020)
 get_ffiec_census_file(years=year_list)
 
@@ -74,11 +79,53 @@ for folder in directories:
     os.rmdir(DATA_PATH + folder)
 
 
-# In[ ]:
+# In[18]:
 
 
 #extract county list for 2004-2019 (include other relevant census data)
 #MSA/MD, state, demographic, income, housing
+def get_census_fields(field_dict={}, census_file="census2018.csv", data_path=DATA_PATH):
+    """
+    Extracts the enumerated fields from an FFIEC Census data CSV
+    Returns a pandas dataframe with the selected fields
+    Note: not all FFIEC census data is in CSV format and data contents may change between years
+    field_dict: a name, field number dictionary. The keys will be used as column names, 
+        the values will be used to select fields from the FFIEC Census file
+    The data file documentation is not zero-indexed. 
+    Pass in the index in the documentation. Do not adjust for zero-indexing.
+    """
+    #field_dict = {field_dict[field]-1 for field in field_dict.keys()} #adjust for zero indexing in data
+    field_names = list(field_dict.keys())
+    field_nums = list(field_dict.values())
+    field_nums = [num-1 for num in field_nums]
+    print(field_names)
+    print(field_nums)
+    census_data = pd.read_csv(data_path+census_file, usecols=field_nums, names=field_names, 
+                              header=None, dtype=object)
+    #data are loaded as objects to preserve integrity of geographic identifiers with leading 0-s
+    return census_data
+
+field_dict = {"msamd":2, "state_fips":3, "county_fips":4, "tract":5, "small_county":7,
+             "urban_rural":10, "msa_med_fam_inc":11, "med_household_inc":12, 
+             "tract_med_fam_inc_as_pct_msamd":13, "ffiec_med_fam_inc":14, "total_persons":16,
+             "total_households":17, "minority_pop_pct":21,"urban_rural_pop":76,
+             "urban_pop":77, "urbanized_pop":78, "urban_cluster_pop":79, "rural_pop":80,
+             "total_households":360, "households_under_10":361, "households_10k_15k":362,
+             "households_15k_20k":363, "households_20k_25k":364, "households_25k_30k":365,
+             "households_30k_35k":366, "households_35_40k":367, "households_40k_45k":368,
+             "households_45_50k":369, "households_50k_60k":370, "households_60k_75k":371,
+             "households_75k_100k":372, "households_100k_125k":373, "households_125k_150k":374,
+            "households_150k_200k":375, "households_over_200k":376}
+
+
+census_data = get_census_fields(field_dict=field_dict)
+census_data.to_csv(DATA_PATH+"census_2018_subset.txt", index=False, sep="|")
+census_data.head()
+
+
+# In[ ]:
+
+
 
 
 
--- python/hmda_db.py
+++ python/hmda_db.py
@@ -1,34 +1,85 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[53]:
 
+
+import io
+import os
+from os import path
+from os import listdir
+from os.path import isfile, join
 
 import pandas as pd
 import requests
+import zipfile
+
+low_memory=False
+
+DATA_PATH = "../data/"
+CENSUS_DATA_PATH = "../data/census/"
+DB_COUNTY_PATH = "../data/hmda_county_data/"
+DB_STATE_PATH = "../data/hmda_state_data/"
+
+if not os.path.exists(DB_COUNTY_PATH):
+    os.makedirs(DB_COUNTY_PATH)
+if not os.path.exists(DB_STATE_PATH):
+    os.makedirs(DB_STATE_PATH)
+    
+state_codes_rev = {
+  '01':'AL', '02':'AK', '04':'AZ', '05':'AR', '06':'CA', '08':'CO', '09':'CT', 
+  '10':'DE', '11':'DC', '12':'FL', '13':'GA', '15':'HI', '16':'ID', '17':'IL',
+  '18':'IN', '19':'IA', '20':'KS', '21':'KY', '22':'LA', '23':'ME', '24':'MD', 
+  '25':'MA', '26':'MI', '27':'MN', '28':'MS', '29':'MO', '30':'MT', '31':'NE',
+  '32':'NV', '33':'NH', '34':'NJ', '35':'NM', '36':'NY', '37':'NC', '38':'ND',
+  '39':'OH', '40':'OK', '41':'OR', '42':'PA', '44':'RI', '45':'SC', '46':'SD',
+  '47':'TN', '48':'TX', '49':'UT', '50':'VT', '51':'VA', '53':'WA', '54':'WV',
+  '55':'WI', '56':'WY', '60':'AS', '72':'PR', '78':'VI', '66':'GU', '69':'MP'
+}
 
 
-# In[ ]:
+# In[46]:
 
 
+#download TS 2018
+#download Panel 2018
+#download MSA MD map
+ts_url = "https://s3.amazonaws.com/cfpb-hmda-public/prod/snapshot-data/2018/2018_public_ts_pipe.zip"
+panel_url = "https://s3.amazonaws.com/cfpb-hmda-public/prod/snapshot-data/2018/2018_public_panel_pipe.zip"
+msa_md_url = "https://s3.amazonaws.com/cfpb-hmda-public/prod/snapshot-data/2018/2018_public_msamd_pipe.zip"
 
+ts_resp = requests.get(ts_url)
+with open(DATA_PATH+"2018_public_ts_pipe.zip", "wb") as infile:
+            infile.write(ts_resp.content)
+        
+panel_resp = requests.get(panel_url)
+with open(DATA_PATH+"2018_public_panel_pipe.zip", "wb") as infile:
+            infile.write(panel_resp.content)
+        
+msa_resp = requests.get(msa_md_url)
+with open(DATA_PATH+"2018_public_msamd_pipe.zip", "wb") as infile:
+            infile.write(msa_resp.content)
+#write files for use in analysis
+with zipfile.ZipFile(DATA_PATH+"2018_public_ts_pipe.zip", 'r') as zip_ref:
+    zip_ref.extractall(DATA_PATH)
+with zipfile.ZipFile(DATA_PATH+"2018_public_panel_pipe.zip", 'r') as zip_ref:
+    zip_ref.extractall(DATA_PATH)
+with zipfile.ZipFile(DATA_PATH+"2018_public_msamd_pipe.zip", 'r') as zip_ref:
+    zip_ref.extractall(DATA_PATH)
+
+#remove zip archives
+os.remove(DATA_PATH+"2018_public_ts_pipe.zip")
+os.remove(DATA_PATH+"2018_public_panel_pipe.zip")
+os.remove(DATA_PATH+"2018_public_msamd_pipe.zip")
 
 
 # In[47]:
 
 
 #get HMDA data for 2018 from DB API
-#base url: https://ffiec.cfpb.gov/data-browser
-#url with 1 county: https://ffiec.cfpb.gov/data-browser/data/2018?category=counties&items=30029
-#url with 1 county and 1 lei: https://ffiec.cfpb.gov/data-browser/data/2018?category=counties&items=30029&leis=549300XQVJ1XBNFA5536
-#above with single family and manufactured dwelling categories: https://ffiec.cfpb.gov/data-browser/data/2018?category=counties&items=30029&leis=549300XQVJ1XBNFA5536&dwelling_categories=Single%20Family%20(1-4%20Units)%3ASite-Built,Single%20Family%20(1-4%20Units)%3AManufactured
-#above with home purchase as purpose
-
-#pull data for all counties of interest
-#create aggregates
-#map aggregates
+#available Data Browser endpoints: filers, aggrgation, csv
 def get_hmda_db_data(filter1="", filter2="", filter1_vals=[], filter2_vals=[], leis=[],
-                     geo_type="counties", geo_list=[], year=2018, file_name="hmda_data.csv"):
+                     geo_type="counties", geo_list=[], years=[2018], file_name="hmda_data.csv"):
     """
     The Data Browser accepts 2 filters and enumerations for each.
     Filter1: the first chosen filter (string)
@@ -43,49 +94,76 @@ def get_hmda_db_data(filter1="", filter2="", filter1_vals=[], filter2_vals=[], l
     
     geo_type: counties, state, msamds, nationwide. Note: only a single geo_type may be chosen. 
     All values in the geo_list must be of the chosen type
-    geo_list: a list of numeric codes for the chosen geo_type
+    geo_list: states use letter codes, MSAMDs and counties use 5 digit FIPS codes
     
     Note: when selecting multiple geographies the data selection is expanded using OR logic 
     while non-geographic filters use AND operator logic, each additional filter reduces the data returned.
     """
-    db_base_url = "https://ffiec.cfpb.gov/data-browser/data/{year}?category={geo_type}".format(year=year, geo_type=geo_type)
+    #curl --location --request GET '
+    #"https://ffiec.cfpb.gov/v2/data-browser-api/view/csv?states=CA,MD,DC&years=2018&actions_taken=5"
+    #db_base_url = "https://ffiec.cfpb.gov/data-browser/data/{year}?category={geo_type}".format(year=year, geo_type=geo_type)
 
     if geo_type == "nationwide":
         if len(geo_list) > 0:
             print("ignoring geo list, retrieving nationwide data")
-        if len(leis) > 0:
-            db_base_url = db_base_url + "&leis=" + ",".join([str(lei) for lei in leis])
-        if len(filter1_vals) > 0:
-            db_base_url = db_base_url + "&" + filter1 + ",".join([str(val) for val in filter1_vals])
-        if len(filter2_vals) > 0:
-            db_base_url = db_base_url + "&" + filter1 + ",".join([str(val) for val in filter2_vals])
-        db_resp = requests.get(db_base_url)
-        
+            geo_list = []
+        db_base_url = "https://hmda4.demo.cfpb.gov/v2/data-browser-api/view/nationwide/csv?"
     else:
-        db_base_url = db_base_url + "&items=" + ",".join([str(geo) for geo in geo_list])
-        if len(leis) > 0:
-            db_base_url = db_base_url + "&leis=" + ",".join([str(lei) for lei in leis])
-        if len(filter1_vals) > 0:
-            db_base_url = db_base_url + "&" + filter1 + "=" + ",".join([str(val) for val in filter1_vals])
-        if len(filter2_vals) > 0:
-            db_base_url = db_base_url + "&" + filter1 + ",".join([str(val) for val in filter2_vals])
-        db_resp = requests.get(db_base_url)      
+        db_base_url = "https://ffiec.cfpb.gov/v2/data-browser-api/view/csv?"
+        db_base_url = db_base_url + geo_type + "=" + ",".join([str(geo) for geo in geo_list])
+    
+    db_base_url = db_base_url + "&years" + "=" + ",".join([str(year) for year in years])
+    if len(leis) > 0:
+        db_base_url = db_base_url + "&leis=" + ",".join([str(lei) for lei in leis])
+    if len(filter1_vals) > 0:
+        db_base_url = db_base_url + "&" + filter1 + "=" + ",".join([str(val) for val in filter1_vals])
+    if len(filter2_vals) > 0:
+        db_base_url = db_base_url + "&" + filter2 + "=" + ",".join([str(val) for val in filter2_vals])
+    db_resp = requests.get(db_base_url)
     print(db_base_url)
     return db_resp
 
 
-# In[50]:
+# In[48]:
 
 
-data = get_hmda_db_data(geo_type="msamds", geo_list=["11500"], filter1="action_taken", filter1_vals=[1])
-data.text
+#load 2018 FFIEC Census data subset
+census_2018_df = pd.read_csv(CENSUS_DATA_PATH + "census_2018_subset.txt", sep="|", dtype=object)
+census_2018_df["county_fips5"] = census_2018_df.apply(lambda x: x.state_fips + x.county_fips, axis=1)
+print(len(census_2018_df.county_fips5.unique()), "counties in FFIEC 2018 Census flat file")
+
+
+# In[52]:
+
+
+state_files = [f[:-4] for f in listdir(DB_STATE_PATH) if isfile(join(DB_STATE_PATH, f))]
+for state in census_2018_df.state_fips.unique():
+    state_letter = state_codes_rev[state]
+    if state_letter not in state_files:
+        state_data = get_hmda_db_data(geo_type="states", geo_list=[state_letter]).content
+        state_df = pd.read_csv(io.StringIO(state_data.decode("utf-8")))
+        state_df.to_csv(DB_STATE_PATH+str(state_letter)+".csv", index=False)
+
+
+# In[56]:
+
+
+#create list of existing files to not re-pull data
+county_files = [f[:-4] for f in listdir(DB_COUNTY_PATH) if isfile(join(DB_COUNTY_PATH, f))]
+
+#pull data for all counties of interest that are not present in DB_DATA_DIR
+for county in census_2018_df.county_fips5.unique():
+    #print("checking for: ", county)
+    if county not in county_files:
+        print("downloading:", county)
+        county_data = get_hmda_db_data(geo_type="counties", geo_list=[county]).content
+        county_df = pd.read_csv(io.StringIO(county_data.decode("utf-8")))
+        county_df.to_csv(DB_COUNTY_PATH+str(county)+".csv", index=False)
+   
 
 
 # In[ ]:
 
 
 
-print("saving data"
-        with open(DATA_PATH+file_name, "wb") as infile:
-            infile.write(data.content)
 
